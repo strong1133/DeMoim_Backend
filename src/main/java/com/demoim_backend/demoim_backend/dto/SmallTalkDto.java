e676cd9a6f3af57c6d8cf16b554f92f4e7fd1907
[strong1133] <[strong1133@naver.com]>
2021-04-30 10:22:01 +0900

    merge

--- src/main/java/com/demoim_backend/demoim_backend/dto/SmallTalkDto.java
+++ src/main/java/com/demoim_backend/demoim_backend/dto/SmallTalkDto.java
@@ -5,6 +5,9 @@ import com.demoim_backend.demoim_backend.model.SmallTalk;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
+
 @Getter
 @RequiredArgsConstructor
 public class SmallTalkDto {
