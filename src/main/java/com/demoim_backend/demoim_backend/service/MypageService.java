f47c2654922f7f39944183b14847f2fb6f949a1e
Jooonyub <59817144+Jooonyub@users.noreply.github.com>
2021-05-13 18:27:54 +0900

    마이페이지_히스토리_참여 프로젝트_조회시에 리더 중복찍힘 수정(1)

--- src/main/java/com/demoim_backend/demoim_backend/service/MypageService.java
+++ src/main/java/com/demoim_backend/demoim_backend/service/MypageService.java
@@ -85,7 +85,6 @@ public class MypageService {
     }
 
     //마이페이지의 히스토리 _ 참여 프로젝트 (진행중 1개 + 참여했던 프로젝트 다수) 조회
-
     public Map<String, Object> findMyActivedTeam(Authentication authentication) {
 //    public List<ActiveTeamResponseDto> findMyActivedTeam(Authentication authentication) {
         // - ActiveTeamResponseDto 타입의 현재 진행중 프로젝트 하나(projectState = ACTIVATED)
@@ -97,9 +96,12 @@ public class MypageService {
         ActiveTeamResponseDto activatedTeamResponseDto = new ActiveTeamResponseDto();
         List<ActiveTeamResponseDto> finishedTeamResponseDtoList = new ArrayList<>();
 
+        //userId를 가지고 멤버로서 참여프로젝트 조회하여 리스트 담기
         List<ApplyInfo> myApplyInfoList = applyInfoRepository.findAllByUserIdAndApplyStateAndMembership(user.getId(), ApplyInfo.ApplyState.ACCEPTED, ApplyInfo.Membership.MEMBER);
         System.out.println("applyInfoList :" + myApplyInfoList);
-        List<ResponseUserDto> memberList = new ArrayList<>();
+        //멤버리스트 생성
+        //memberList for문 안에 넣기 18:12
+//        List<ResponseUserDto> memberList = new ArrayList<>();
 
 
         for (ApplyInfo myApplyInfo : myApplyInfoList) {
@@ -109,21 +111,28 @@ public class MypageService {
             User userInfo = userRepository.findById(myApplyInfo.getUser().getId()).orElseThrow(
                     () -> new IllegalArgumentException("해당 유저가 없습니다.")
             );
+            //for문 안에 memberList 생성(18:19)
+            List<ResponseUserDto> memberList = new ArrayList<>();
 
             //if 현재 진행중인 프로젝트의 경우, else 끝난 프로젝트들
             List<ApplyInfo> membersApplyInfoList = applyInfoRepository.findAllByteamIdAndApplyState(team.getId(), ApplyInfo.ApplyState.ACCEPTED);
 
+            //얘로 뭘 하려했을까.. member와 leader를 가지고 ResponseUserDto를 만드려던 것 아닐까?
             for (ApplyInfo memberApplyInfo : membersApplyInfoList) {
                 User member = memberApplyInfo.getUser();
                 User leader = userRepository.findById(memberApplyInfo.getTeam().getLeader().getId()).orElseThrow(
                         () -> new IllegalArgumentException("해당 유저가 없습니다.")
                 );
+                //위에서 멤버로서 조회했기때문에 리더와 같을 가능성은 없을듯
+
                 ResponseUserDto responseUserDto = ResponseUserDto.builder().build().entityToDto(member);
                 memberList.add(responseUserDto);
-                responseUserDto = ResponseUserDto.builder().build().entityToDto(leader);
-                memberList.add(responseUserDto);
+//                ResponseUserDto responseUserDto = ResponseUserDto.builder().build().entityToDto(leader);
+//                memberList.add(responseUserDto);
+
             }
             System.out.println("memberList :" + memberList);
+
             if (team.getProjectState() == Team.StateNow.ACTIVATED || team.getProjectState() == Team.StateNow.YET) {
                 activatedTeamResponseDto = new ActiveTeamResponseDto(team, memberList);
             } else {
